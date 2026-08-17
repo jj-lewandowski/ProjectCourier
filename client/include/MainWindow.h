2a94e08b8e2b09e4b948d7cb5f01fee6675d1b1d
JJ Lewandowski <jj@anbast.com>
2026-08-17 23:36:38 +0100

    [18] Add comments to existing boilerplate code

--- client/include/MainWindow.h
+++ client/include/MainWindow.h
@@ -1,4 +1,4 @@
-#pragma once
-#include <windows.h>
+#pragma once																	//Pragma once is an instruction to the preprocessor to only link this file once
+#include <windows.h>															//This header file includes features from the Windows32API (the functions/data types/ macros live in other header files)
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //Here we declare this function that is used in MainWindow.cpp and in main.cpp
--- client/src/MainWindow.cpp
+++ client/src/MainWindow.cpp
@@ -1,18 +1,18 @@
-#include "MainWindow.h"
+#include "MainWindow.h"                                                             //Here we include the MainWindow header file that contains the declaration for the WindowProc function
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {   //Here we define the WindowProc function, it is defined by me but the func and var names are common standards
+    switch (uMsg) {                                                                 //Here we use a switch case on the Message that we get from the DispatchMessage function
+    case WM_DESTROY:                                                                //If the Window is being destroyed
+        PostQuitMessage(0);                                                         //Tell the program that it can safely stop and quit
+        return 0;                                                                   //Returns a 0 to signify that the function completed correctly
 
-    case WM_PAINT: {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+    case WM_PAINT: {                                                                //If the Window get a paint message (the system or another application wants to paint a portion of the window
+        PAINTSTRUCT ps;                                                             //Declaration of a Paint Structure with the name ps
+        HDC hdc = BeginPaint(hwnd, &ps);                                            //HDC is a paintbrush and canvas letting the user paint on the screen. It gets the window handle and the address of the PAINTSTRUCT
         // TODO: Render custom Windows 95 UI elements and two-pane layout here
-        EndPaint(hwnd, &ps);
-        return 0;
+        EndPaint(hwnd, &ps);                                                        //We release the resources used by BeginPaint
+        return 0;                                                                   //Returns a 0 to signify that the function completed correctly
     }
-    }
-    return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+    }   
+    return DefWindowProcW(hwnd, uMsg, wParam, lParam);                              //Default Windows built in procedure to handle anything we didn't account for or needed in the switch statement
 }
